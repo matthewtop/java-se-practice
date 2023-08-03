90bc20125d99d818e83d0673a3185965cd1c1766
matthewtop <tolpamateusz@gmail.com>
2023-08-03 15:53:29 +0200

    intermediate ex1

---
+++ src/main/java/pl/globallogic/exercises/intermediate/ex1NumberInWord.java
@@ -0,0 +1,50 @@
+package pl.globallogic.exercises.intermediate;
+
+public class ex1NumberInWord {
+    public static void main(String[] args) {
+        printNumberInWord(0);
+        printNumberInWord(6);
+        printNumberInWord(10);
+    }
+
+    private static void printNumberInWord(int number){
+        if(number<0){
+            System.out.printf("Invalid value");
+        }
+        switch(number){
+            case 0:
+                System.out.println("ZERO");
+                break;
+            case 1:
+                System.out.println("ONE");
+                break;
+            case 2:
+                System.out.println("TWO");
+                break;
+            case 3:
+                System.out.println("THREE");
+                break;
+            case 4:
+                System.out.println("FOUR");
+                break;
+            case 5:
+                System.out.println("FIVE");
+                break;
+            case 6:
+                System.out.println("SIX");
+                break;
+            case 7:
+                System.out.println("SEVEN");
+                break;
+            case 8:
+                System.out.println("EIGHT");
+                break;
+            case 9:
+                System.out.println("NINE");
+                break;
+            default:
+                System.out.println("OTHER");
+                break;
+        }
+    }
+}
