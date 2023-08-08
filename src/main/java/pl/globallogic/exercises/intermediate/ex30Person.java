f1330768b1f8a3a02817ff1e54504ba5d00d2693
matthewtop <tolpamateusz@gmail.com>
2023-08-08 14:37:27 +0200

    updated ex 30 and 31

--- src/main/java/pl/globallogic/exercises/intermediate/ex30Person.java
+++ src/main/java/pl/globallogic/exercises/intermediate/ex30Person.java
@@ -17,44 +17,46 @@ public class ex30Person {
         System.out.println("fullName= " + person.getFullName());
 
     }
-    static class Person{
-        private String firstName;
-        private String lastName;
-        private int age;
 
-        public String getFirstName(){
-            return firstName;
-        }
-        public String getLastName(){
-            return lastName;
-        }
-        public int getAge(){
-            return age;
-        }
-        public void setFirstName(String firstName){
-            this.firstName = firstName;
-        }
-        public void setLastName(String lastName){
-            this.lastName=lastName;
-        }
-        public void setAge(int age){
-            if(age<0||age>100){
-                age=0;
-            }
-            this.age=age;
-        }
-        public boolean isTeen(){
-            getAge();
-            return (age>12&&age<20);
-        }
-        public String getFullName(){
-            getFirstName();
-            getLastName();
-            if(firstName.isEmpty()&&lastName.isEmpty()){
-                return "";
-            }
-            return firstName+lastName;
-        }
+}
 
+class Person{
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName(){
+        return firstName;
     }
+    public String getLastName(){
+        return lastName;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName=lastName;
+    }
+    public void setAge(int age){
+        if(age<0||age>100){
+            age=0;
+        }
+        this.age=age;
+    }
+    public boolean isTeen(){
+        getAge();
+        return (age>12&&age<20);
+    }
+    public String getFullName(){
+        getFirstName();
+        getLastName();
+        if(firstName.isEmpty()&&lastName.isEmpty()){
+            return "";
+        }
+        return firstName+lastName;
+    }
+
 }
--- src/main/java/pl/globallogic/exercises/intermediate/ex31WallArea.java
+++ src/main/java/pl/globallogic/exercises/intermediate/ex31WallArea.java
@@ -10,55 +10,55 @@ public class ex31WallArea {
         System.out.println("area= " + wall.getArea());
 
     }
+}
 
-    static class Wall{
-        private double width;
-        private double height;
+class Wall{
+    private double width;
+    private double height;
 
-        public Wall(){
+    public Wall(){
+    }
+    public Wall(double width,double height){
+        if(width<0){
+            this.width= width;
+            width=0;
         }
-        public Wall(double width,double height){
-            if(width<0){
-                this.width= width;
-                width=0;
-            }
-            if(height<0){
-                this.height=height;
-                height=0;
-            }
-            this.width=width;
+        if(height<0){
             this.height=height;
+            height=0;
         }
-
-        public double getWidth(){
-            return width;
-        }
-        public double getHeight(){
-            return height;
-        }
-        public void setWidth(double width){
-            if(width<0){
-                width=0;
-            }
-            else {
-                this.width = width;
-            }
-        }
-        public void setHeight(double height){
-            if(height<0){
-                this.height= 0;
-            }
-            else {
-                this.height = height;
-            }
-        }
-        public double getArea(){
-            getHeight();
-            getWidth();
-            return width*height;
-        }
-
-
+        this.width=width;
+        this.height=height;
     }
 
+    public double getWidth(){
+        return width;
+    }
+    public double getHeight(){
+        return height;
+    }
+    public void setWidth(double width){
+        if(width<0){
+            width=0;
+        }
+        else {
+            this.width = width;
+        }
+    }
+    public void setHeight(double height){
+        if(height<0){
+            this.height= 0;
+        }
+        else {
+            this.height = height;
+        }
+    }
+    public double getArea(){
+        getHeight();
+        getWidth();
+        return width*height;
+    }
+
+
 }
+
