83b0ef9339935d0320b6de353a66fc7ab083c70f
matthewtop <tolpamateusz@gmail.com>
2023-08-16 17:14:39 +0200

    ex 41

--- src/main/java/pl/globallogic/lessons/lesson9/interfaces/Box.java
+++ src/main/java/pl/globallogic/lessons/lesson9/interfaces/Box.java
@@ -1,7 +1,7 @@
 package pl.globallogic.lessons.lesson9.interfaces;
 
-public class Box extends Number{
-
-    private T payload;
-    public Box(payload)
-}
+//public class Box extends Number{
+//
+//    private T payload;
+//    public <payload> Box(payload);
+//}
