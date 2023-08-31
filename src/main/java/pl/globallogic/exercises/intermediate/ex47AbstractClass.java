a817f541c6677e41d0a203a038f3df23e344cc63
matthewtop <tolpamateusz@gmail.com>
2023-09-01 00:01:02 +0200

    ex47

--- src/main/java/pl/globallogic/exercises/intermediate/ex47AbstractClass.java
+++ src/main/java/pl/globallogic/exercises/intermediate/ex47AbstractClass.java
@@ -1,4 +1,4 @@
-//package pl.globallogic.exercises.intermediate;
+package pl.globallogic.exercises.intermediate;
 //Create an abstract class to define items that can be stored in a tree.
 //        Implement the following:
 //        1. ListItem (abstract class)
@@ -74,261 +74,281 @@
 //        involve following a null reference.
 //        Once again, duplicates are not allowed.
 //
-//public class ex47AbstractClass {
-//}
-//
-//
-//abstract class ListItem{
-//    protected ListItem rightLink;
-//    protected ListItem leftLink;
-//    protected Object value;
-//
-//    public ListItem(Object value){
-//        this.value=value;
-//    }
-//
-//    abstract ListItem next();
-//    abstract ListItem setNext(ListItem item);
-//    abstract ListItem previous();
-//    abstract ListItem setPrevious(ListItem item);
-//    abstract int compareTo(ListItem item);
-//
-//    public Object getValue(){
-//        return value;
-//    }
-//
-//    public void setValue(Object value){
-//        this.value=value;
-//    }
-//
-//}
-//
-//class Node extends ListItem{
-//    Node(Object value){
-//        super(value);
-//    }
-//
-//    @Override
-//    ListItem next(){
-//        return this.rightLink;
-//    }
-//    @Override
-//    ListItem setNext(ListItem item){
-//        this.rightLink=item;
-//        return this.rightLink;
-//    }
-//    @Override
-//    ListItem previous(){
-//        return this.leftLink;
-//    }
-//    @Override
-//    ListItem setPrevious(ListItem item){
-//        this.leftLink=item;
-//        return this.leftLink;
-//    }
-//    @Override
-//    int compareTo(ListItem item) {
-//        if (item != null) {
-//            return ((String) super.getValue()).compareTo((String) item.getValue());
-//        } else {
-//            return -1;
-//        }
-//    }
-//}
-//
-//interface NodeList{
-//    void getRoot();
-//    boolean addItem(ListItem item);
-//    boolean removeItem(ListItem item);
-//    void traverse(ListItem root);
-//}
-//
-//
-//class MyLinkedList implements NodeList{
-//    private ListItem root;
-//
-//    public MyLinkedList(ListItem root){
-//        this.root=root;
-//    }
-//
-//    @Override
-//    public void getRoot(){
-//        return root;
-//    }
-//
-//    @Override
-//    public boolean addItem(ListItem item) {
-//        if(this.root == null){
-//            this.root = item;
-//            return true;
-//        }
-//
-//        ListItem currentItem = this.root;
-//        while(currentItem != null){
-//            int comparison = (currentItem.compareTo(item));
-//            if (comparison < 0){
-//                if(currentItem.next() != null){
-//                    currentItem = currentItem.next();
-//                } else {
-//                    currentItem.setNext(item);
-//                    item.setPrevious(currentItem);
-//                    return true;
-//                }
-//            }
-//        }
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean removeItem(ListItem item){
-//        if (item != null) {
-//            ListItem currentItem = this.root;
-//            ListItem parentItem = currentItem;
-//
-//            while (currentItem != null) {
-//                int comparison = currentItem.compareTo(item);
-//                if (comparison == 0) {
-//                    performRemoval(currentItem, parentItem);
-//                    return true;
-//                } else if (comparison < 0) {
-//                    parentItem = currentItem;
-//                    currentItem = currentItem.next();
-//                } else {
-//                    parentItem = currentItem;
-//                    currentItem = currentItem.previous();
-//                }
-//            }
-//        }
-//        return false;
-//    }
-//
-//
-//    private void performRemoval(ListItem item, ListItem parent) {
-//        if (item.next() == null) {
-//            if (parent.next() == item) {
-//                parent.setNext(item.previous());
-//            } else if (parent.previous() == item) {
-//                parent.setPrevious(item.previous());
-//            } else {
-//                root = item.previous();
-//            }
-//        } else if (item.previous() == null) {
-//            if (parent.next() == item) {
-//                parent.setNext(item.next());
-//            } else if (parent.previous() == item) {
-//                parent.setPrevious(item.next());
-//            } else {
-//                root = item.next();
-//            }
-//        } else {
-//            ListItem current = item.next();
-//            ListItem leftmostParent = item;
-//            while (current.previous() != null) {
-//                leftmostParent = current;
-//                current = current.previous();
-//            }
-//            item.setValue(current.getValue());
-//            if (leftmostParent == item) {
-//                item.setNext(current.next());
-//            } else {
-//                leftmostParent.setPrevious(current.next());
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public void traverse(ListItem root){
-//        if (root==null){
-//            System.out.println("The list is empty");
-//            return;
-//        }
-//        if (root.previous() !=null){
-//            traverse(root.previous());
-//        }
-//        System.out.println(root.getValue());
-//        if (root.next() != null){
-//            traverse(root.next());
-//        }
-//    }
-//
-//}
-//class SearchTree implements NodeList{
-//    private ListItem root;
-//    public  SearchTree(ListItem root){
-//        this.root=root;
-//    }
-//
-//    @Override
-//    public ListItem getRoot(){
-//        return root;
-//    }
-//
-//    @Override
-//    public boolean addItem(ListItem newItem){
-//        if (root==null){
-//            root=newItem;
-//            return true;
-//        }
-//
-//        ListItem currentItem = root;
-//        while (currentItem !=null){
-//            int comparison = currentItem.compareTo(newItem);
-//            if (comparison ==0){
-//                return false;
-//            } else if (comparison<0) {
-//                if (currentItem.next() != null){
-//                    currentItem = currentItem.next();
-//                }
-//                else {
-//                    currentItem.setNext(newItem);
-//                    return true;
-//                }
-//            }
-//            else {
-//                if (currentItem.previous() !=null){
-//                    currentItem = currentItem.previous();
-//                }
-//                else {
-//                    currentItem.setPrevious(newItem);
-//                    return true;
-//                }
-//            }
-//        }
-//        return false;
-//    }
-//
-//    private void performRemoval(ListItem item, ListItem parent) {
-//        if (item.next() == null) {
-//            if (parent.next() == item) {
-//                parent.setNext(item.previous());
-//            } else if (parent.previous() == item) {
-//                parent.setPrevious(item.previous());
-//            } else {
-//                root = item.previous();
-//            }
-//        } else if (item.previous() == null) {
-//            if (parent.next() == item) {
-//                parent.setNext(item.next());
-//            } else if (parent.previous() == item) {
-//                parent.setPrevious(item.next());
-//            } else {
-//                root = item.next();
-//            }
-//        } else {
-//            ListItem current = item.next();
-//            ListItem leftmostParent = item;
-//            while (current.previous() != null) {
-//                leftmostParent = current;
-//                current = current.previous();
-//            }
-//            item.setValue(current.getValue());
-//            if (leftmostParent == item) {
-//                item.setNext(current.next());
-//            } else {
-//                leftmostParent.setPrevious(current.next());
-//            }
-//        }
-//    }
-//
-//
-//}
+public class ex47AbstractClass {
+}
+
+
+abstract class ListItem{
+    protected ListItem rightLink;
+    protected ListItem leftLink;
+    protected Object value;
+
+    public ListItem(Object value){
+        this.value=value;
+    }
+
+    abstract ListItem next();
+    abstract ListItem setNext(ListItem item);
+    abstract ListItem previous();
+    abstract ListItem setPrevious(ListItem item);
+    abstract int compareTo(ListItem item);
+
+    public Object getValue(){
+        return value;
+    }
+
+    public void setValue(Object value){
+        this.value=value;
+    }
+}
+
+class Node extends ListItem{
+    Node(Object value){
+        super(value);
+    }
+
+    @Override
+    ListItem next(){
+        return this.rightLink;
+    }
+    @Override
+    ListItem setNext(ListItem item){
+        this.rightLink=item;
+        return this.rightLink;
+    }
+    @Override
+    ListItem previous(){
+        return this.leftLink;
+    }
+    @Override
+    ListItem setPrevious(ListItem item){
+        this.leftLink=item;
+        return this.leftLink;
+    }
+    @Override
+    int compareTo(ListItem item) {
+        if (item != null) {
+            return ((String) super.getValue()).compareTo((String) item.getValue());
+        } else {
+            return -1;
+        }
+    }
+}
+
+interface NodeList{
+    ListItem getRoot();
+    boolean addItem(ListItem item);
+    boolean removeItem(ListItem item);
+    void traverse(ListItem root);
+
+    void performRemoval(ListItem item, ListItem parent);
+}
+
+class MyLinkedList implements NodeList{
+    private ListItem root;
+
+    public MyLinkedList(ListItem root){
+        this.root=root;
+    }
+
+    @Override
+    public ListItem getRoot(){
+        return root;
+    }
+
+    @Override
+    public boolean addItem(ListItem item) {
+        if(this.root == null){
+            this.root = item;
+            return true;
+        }
+
+        ListItem currentItem = this.root;
+        while(currentItem != null){
+            int comparison = (currentItem.compareTo(item));
+            if (comparison < 0){
+                if(currentItem.next() != null){
+                    currentItem = currentItem.next();
+                } else {
+                    currentItem.setNext(item);
+                    item.setPrevious(currentItem);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item){
+        if (item != null) {
+            ListItem currentItem = this.root;
+            ListItem parentItem = currentItem;
+
+            while (currentItem != null) {
+                int comparison = currentItem.compareTo(item);
+                if (comparison == 0) {
+                    performRemoval(currentItem, parentItem);
+                    return true;
+                } else if (comparison < 0) {
+                    parentItem = currentItem;
+                    currentItem = currentItem.next();
+                } else {
+                    parentItem = currentItem;
+                    currentItem = currentItem.previous();
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public void performRemoval(ListItem item, ListItem parent) {
+        if (item.next() == null) {
+            if (parent.next() == item) {
+                parent.setNext(item.previous());
+            } else if (parent.previous() == item) {
+                parent.setPrevious(item.previous());
+            } else {
+                root = item.previous();
+            }
+        } else if (item.previous() == null) {
+            if (parent.next() == item) {
+                parent.setNext(item.next());
+            } else if (parent.previous() == item) {
+                parent.setPrevious(item.next());
+            } else {
+                root = item.next();
+            }
+        } else {
+            ListItem current = item.next();
+            ListItem leftmostParent = item;
+            while (current.previous() != null) {
+                leftmostParent = current;
+                current = current.previous();
+            }
+            item.setValue(current.getValue());
+            if (leftmostParent == item) {
+                item.setNext(current.next());
+            } else {
+                leftmostParent.setPrevious(current.next());
+            }
+        }
+    }
+
+    @Override
+    public void traverse(ListItem root){
+        if (root==null){
+            System.out.println("The list is empty");
+            return;
+        }
+        if (root.previous() !=null){
+            traverse(root.previous());
+        }
+        System.out.println(root.getValue());
+        if (root.next() != null){
+            traverse(root.next());
+        }
+    }
+
+}
+class SearchTree implements NodeList{
+    private ListItem root;
+    public  SearchTree(ListItem root){
+        this.root=root;
+    }
+
+    @Override
+    public ListItem getRoot(){
+        return root;
+    }
+
+    @Override
+    public boolean addItem(ListItem newItem){
+        if (root==null){
+            root=newItem;
+            return true;
+        }
+
+        ListItem currentItem = root;
+        while (currentItem !=null){
+            int comparison = currentItem.compareTo(newItem);
+            if (comparison ==0){
+                return false;
+            } else if (comparison<0) {
+                if (currentItem.next() != null){
+                    currentItem = currentItem.next();
+                }
+                else {
+                    currentItem.setNext(newItem);
+                    return true;
+                }
+            }
+            else {
+                if (currentItem.previous() !=null){
+                    currentItem = currentItem.previous();
+                }
+                else {
+                    currentItem.setPrevious(newItem);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+        if (root == null) {
+            System.out.println("The tree is empty");
+            return;
+        }
+
+        if (root.previous() != null) {
+            traverse(root.previous());
+        }
+        System.out.println(root.getValue());
+        if (root.next() != null) {
+            traverse(root.next());
+        }
+    }
+
+    @Override
+    public void performRemoval(ListItem item, ListItem parent) {
+        if (item.next() == null) {
+            if (parent.next() == item) {
+                parent.setNext(item.previous());
+            } else if (parent.previous() == item) {
+                parent.setPrevious(item.previous());
+            } else {
+                root = item.previous();
+            }
+        } else if (item.previous() == null) {
+            if (parent.next() == item) {
+                parent.setNext(item.next());
+            } else if (parent.previous() == item) {
+                parent.setPrevious(item.next());
+            } else {
+                root = item.next();
+            }
+        } else {
+            ListItem current = item.next();
+            ListItem leftmostParent = item;
+            while (current.previous() != null) {
+                leftmostParent = current;
+                current = current.previous();
+            }
+            item.setValue(current.getValue());
+            if (leftmostParent == item) {
+                item.setNext(current.next());
+            } else {
+                leftmostParent.setPrevious(current.next());
+            }
+        }
+    }
+}
